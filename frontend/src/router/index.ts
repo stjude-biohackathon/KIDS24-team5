1ab1cee8c782a4812ee120c2e252e4dffda816af
Aman Patel <amanpatel@gmail.com>
2024-09-05 17:58:57 -0500

    Refactored in to child routes
    Signed-off-by: Aman Patel <amanpatel@gmail.com>

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SubmitManuscript from '../views/SubmitManuscript.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '',
       name: 'home',
       component: HomeView
     },
@@ -15,7 +16,19 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SubmitManuscript.vue')
+      component: SubmitManuscript,
+      children: [
+        {
+          path: '', // Adjusted path
+          name: 'submit-ms-1-submit',
+          component: () => import('../views/steps/step1-submit.vue')
+        },
+        {
+          name: '2-outline',
+          path: '2-outline', // Adjusted path
+          component: () => import('../views/steps/step2-outline.vue')
+        }
+      ]
     }
   ]
 })
---
+++ frontend/src/views/steps/step2-outline.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Outline</h1>
+    <pre>{{ hierarchy }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useManuscriptStore } from '@/stores/manuscript'
+
+const manuscriptStore = useManuscriptStore()
+const hierarchy = computed(() => manuscriptStore.hierarchy)
+</script>
